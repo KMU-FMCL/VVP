0ef67766a13e79156b1b30af3b2b380dd6d7c644
taehun-kmu <taehun@kookmin.ac.kr>
2025-06-05 14:41:52 +0900

    feat(estimation): Implement definitions for types

---
+++ src/estimation/Types.cpp
@@ -0,0 +1,19 @@
+#include "vvp/estimation/Types.h"
+#include <cmath>
+
+namespace vv {
+
+// 상수 정의
+absl::string_view const TimeConstants::kIsoFormat = "%Y%m%d_%H%M%S";
+
+// Implementation is only used as needed
+// Currently mostly empty as most functionality is implemented inline in the
+// header
+
+void VVResult::update_acceleration() {
+  angle_rad = angle * PhysicsConstants::kDegreesToRadians;
+  acc_x = PhysicsConstants::kGravityAcceleration * std::cos(angle_rad);
+  acc_y = PhysicsConstants::kGravityAcceleration * std::sin(angle_rad);
+}
+
+}  // namespace vv
