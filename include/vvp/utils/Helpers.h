a1567b6031944a134470b9a3ce3fc600375a909d
taehun-kmu <taehun@kookmin.ac.kr>
2025-06-05 16:30:48 +0900

    style(utils): Modernize Helpers.h with absl::Span, C++17 ns, and style updates

--- include/vvp/utils/Helpers.h
+++ include/vvp/utils/Helpers.h
@@ -1,39 +1,37 @@
 #ifndef VVP_UTILS_HELPERS_H_
 #define VVP_UTILS_HELPERS_H_
 
+#include "absl/strings/string_view.h"
+#include "absl/time/time.h"
+#include "absl/types/span.h"
+#include "vvp/estimation/Types.h"
 #include <algorithm>
 #include <ctime>
 #include <numeric>
 #include <string>
 
-#include "vvp/estimation/Types.h"
-
-#include "absl/strings/string_view.h"
-#include "absl/time/time.h"
-
-namespace vv {
-namespace utils {
+namespace vv::utils {
 
 /**
  * @brief 명령줄 인자에서 설정 파싱
- * @param argc 인자 개수
- * @param argv 인자 배열
+ * @param args 명령줄 인자 배열
  * @return 파싱된 설정
  */
-Config parse_command_line_args(int argc, char* argv[]);
+auto parse_command_line_args(absl::Span<char* const> args) -> Config;
 
 /**
  * @brief 현재 시간 포매팅
  * @param format 시간 포맷 문자열
  * @return 포매팅된 시간 문자열
  */
-std::string format_current_time(absl::string_view format = kIsoTimeFormat);
+auto format_current_time(
+    absl::string_view format = vv::TimeConstants::kIsoFormat) -> std::string;
 
 /**
  * @brief 현재 날짜를 YYYYMMDD 형식의 문자열로 반환
  * @return "YYYYMMDD" 형식의 날짜 문자열
  */
-std::string get_current_date_string();
+auto get_current_date_string() -> std::string;
 
 /**
  * @brief 도움말 메시지 출력
@@ -53,7 +51,7 @@ void print_opencv_info();
  * @return 최대값의 인덱스
  */
 template <typename T>
-int argmax(const std::vector<T>& vec, int startIdx, int endIdx);
+int argmax(std::vector<T> const& vec, int startIdx, int endIdx);
 
 /**
  * @brief 벡터의 상위 N개 인덱스 찾기
@@ -64,32 +62,26 @@ int argmax(const std::vector<T>& vec, int startIdx, int endIdx);
  * @return 상위 N개 인덱스 배열
  */
 template <typename T>
-std::vector<int> argmax_n(const std::vector<T>& vec,
-                          int n,
-                          int startIdx,
+std::vector<int> argmax_n(std::vector<T> const& vec, int n, int startIdx,
                           int endIdx);
 
-}  // namespace utils
-}  // namespace vv
+}  // namespace vv::utils
 
 // 템플릿 함수 구현
 template <typename T>
-int vv::utils::argmax(const std::vector<T>& vec, int startIdx, int endIdx) {
+int vv::utils::argmax(std::vector<T> const& vec, int startIdx, int endIdx) {
   if (vec.empty() || startIdx < 0 || endIdx > static_cast<int>(vec.size()) ||
       startIdx >= endIdx) {
     return -1;
   }
 
-  return std::distance(
-      vec.begin(),
-      std::max_element(vec.begin() + startIdx, vec.begin() + endIdx));
+  return std::distance(vec.begin(), std::max_element(vec.begin() + startIdx,
+                                                     vec.begin() + endIdx));
 }
 
 template <typename T>
-std::vector<int> vv::utils::argmax_n(const std::vector<T>& vec,
-                                     int n,
-                                     int startIdx,
-                                     int endIdx) {
+std::vector<int> vv::utils::argmax_n(std::vector<T> const& vec, int n,
+                                     int startIdx, int endIdx) {
   if (vec.empty() || startIdx < 0 || endIdx > static_cast<int>(vec.size()) ||
       startIdx >= endIdx || n <= 0) {
     return {};
@@ -101,8 +93,7 @@ std::vector<int> vv::utils::argmax_n(const std::vector<T>& vec,
   std::partial_sort(
       indices.begin(),
       indices.begin() + std::min(n, static_cast<int>(indices.size())),
-      indices.end(),
-      [&vec](int i1, int i2) { return vec[i1] > vec[i2]; });
+      indices.end(), [&vec](int i1, int i2) { return vec[i1] > vec[i2]; });
 
   indices.resize(std::min(n, static_cast<int>(indices.size())));
   return indices;
