8824cd2e092bc0284648b55032c4e9451d9a17f1
taehun-kmu <taehun@kookmin.ac.kr>
2025-06-05 16:29:26 +0900

    refactor(utils): Refactor ConfigLoader::load with helpers, update namespace

--- src/utils/ConfigLoader.cpp
+++ src/utils/ConfigLoader.cpp
@@ -1,75 +1,82 @@
 #include "vvp/utils/ConfigLoader.h"
-
-#include <yaml-cpp/yaml.h>
-
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/str_cat.h"
+#include <yaml-cpp/yaml.h>
 
-namespace vv {
-namespace utils {
+namespace vv::utils {
 
-absl::StatusOr<ConfigAll> ConfigLoader::load(const std::string& filepath) {
+namespace {
+
+// 설정 항목을 안전하게 파싱하는 템플릿 함수
+template <typename T, typename ConfigType>
+void parse_config_item(const YAML::Node& node, std::string const& key,
+                       ConfigType& config, T ConfigType::* member) {
+  if (node[key]) {
+    config.*member = node[key].as<T>();
+  }
+}
+
+// IO 관련 설정 파싱
+void parse_io_config(const YAML::Node& io_node, Config& io_config) {
+  parse_config_item<bool>(io_node, "use_camera", io_config,
+                          &Config::use_camera);
+  parse_config_item<int>(io_node, "camera_port", io_config,
+                         &Config::camera_port);
+  parse_config_item<std::string>(io_node, "input_file_path", io_config,
+                                 &Config::input_file_path);
+  parse_config_item<int>(io_node, "scale", io_config, &Config::scale);
+  parse_config_item<bool>(io_node, "save_results", io_config,
+                          &Config::save_results);
+}
+
+// HOG 관련 설정 파싱
+void parse_hog_config(const YAML::Node& hog_node, HOGParams& hog_config) {
+  parse_config_item<int>(hog_node, "bin_count", hog_config,
+                         &HOGParams::bin_count);
+  parse_config_item<double>(hog_node, "threshold_value", hog_config,
+                            &HOGParams::threshold_value);
+  parse_config_item<int>(hog_node, "blur_kernel_size", hog_config,
+                         &HOGParams::blur_kernel_size);
+  parse_config_item<double>(hog_node, "blur_sigma", hog_config,
+                            &HOGParams::blur_sigma);
+  parse_config_item<int>(hog_node, "erode_kernel_size", hog_config,
+                         &HOGParams::erode_kernel_size);
+}
+
+// VV 추정기 관련 설정 파싱
+void parse_vv_config(const YAML::Node& vv_node, VVParams& vv_config) {
+  parse_config_item<int>(vv_node, "min_angle", vv_config, &VVParams::min_angle);
+  parse_config_item<int>(vv_node, "max_angle", vv_config, &VVParams::max_angle);
+  parse_config_item<double>(vv_node, "smoothing_factor", vv_config,
+                            &VVParams::smoothing_factor);
+}
+
+}  // namespace
+
+auto ConfigLoader::load(std::string const& filepath)
+    -> absl::StatusOr<ConfigAll> {
   ConfigAll cfg;
   try {
     YAML::Node root = YAML::LoadFile(filepath);
-    // I/O settings
+
+    // 각 설정 섹션 파싱
     if (root["io"]) {
-      auto io = root["io"];
-      if (io["use_camera"]) {
-        cfg.io.use_camera = io["use_camera"].as<bool>();
-      }
-      if (io["camera_port"]) {
-        cfg.io.camera_port = io["camera_port"].as<int>();
-      }
-      if (io["input_file_path"]) {
-        cfg.io.input_file_path = io["input_file_path"].as<std::string>();
-      }
-      if (io["scale"]) {
-        cfg.io.scale = io["scale"].as<int>();
-      }
-      if (io["save_results"]) {
-        cfg.io.save_results = io["save_results"].as<bool>();
-      }
+      parse_io_config(root["io"], cfg.io);
     }
-    // HOG parameters
+
     if (root["hog"]) {
-      auto hog = root["hog"];
-      if (hog["bin_count"]) {
-        cfg.hog.bin_count = hog["bin_count"].as<int>();
-      }
-      if (hog["threshold_value"]) {
-        cfg.hog.threshold_value = hog["threshold_value"].as<double>();
-      }
-      if (hog["blur_kernel_size"]) {
-        cfg.hog.blur_kernel_size = hog["blur_kernel_size"].as<int>();
-      }
-      if (hog["blur_sigma"]) {
-        cfg.hog.blur_sigma = hog["blur_sigma"].as<double>();
-      }
-      if (hog["erode_kernel_size"]) {
-        cfg.hog.erode_kernel_size = hog["erode_kernel_size"].as<int>();
-      }
+      parse_hog_config(root["hog"], cfg.hog);
     }
-    // Visual Vertical estimator parameters
+
     if (root["vv_estimator"]) {
-      auto vv = root["vv_estimator"];
-      if (vv["min_angle"]) {
-        cfg.vv.min_angle = vv["min_angle"].as<int>();
-      }
-      if (vv["max_angle"]) {
-        cfg.vv.max_angle = vv["max_angle"].as<int>();
-      }
-      if (vv["smoothing_factor"]) {
-        cfg.vv.smoothing_factor = vv["smoothing_factor"].as<double>();
-      }
+      parse_vv_config(root["vv_estimator"], cfg.vv);
     }
-  } catch (const std::exception& e) {
+  } catch (std::exception const& e) {
     return absl::InvalidArgumentError(absl::StrCat(
         "Failed to load config file '", filepath, "': ", e.what()));
   }
   return cfg;
 }
 
-}  // namespace utils
-}  // namespace vv
+}  // namespace vv::utils
