44aed5d1856ae9a26bfd0dc136a63be365f11895
taehun-kmu <taehun@kookmin.ac.kr>
2025-06-12 19:02:56 +0900

    feat: Add VV indicator drawing module and fix includes

---
+++ src/visualization/vv_indicator_drawing.cc
@@ -0,0 +1,15 @@
+// Placeholder for vv_indicator_drawing.cc
+
+#include "vvp/visualization/vv_indicator_drawing.h"
+#include "vvp/estimation/types.h"  // For VVResult and related types/constants
+
+namespace vv {
+namespace visualization {
+
+void draw_vv_indicators(cv::Mat& canvas, vv::VVResult const& vv_result,
+                        vv::VVParams const& vv_params) {
+  // Implementation will be moved here
+}
+
+}  // namespace visualization
+}  // namespace vv
