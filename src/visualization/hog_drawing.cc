acac4445f5e1c9a785e781f4063e9e9c21a4dc97
taehun-kmu <taehun@kookmin.ac.kr>
2025-06-12 22:38:54 +0900

    refactor(hog_drawing): Extract helper to reduce duplication

--- src/visualization/hog_drawing.cc
+++ src/visualization/hog_drawing.cc
@@ -4,26 +4,28 @@
 #include "vvp/processing/constants.h"  // HOG related constants, ImageConstants
 #include <opencv2/imgproc.hpp>         // For cv::cvtColor, cv::hconcat
 
+namespace {
+// Helper function to convert a single-channel matrix to a displayable BGR
+// image.
+cv::Mat convert_to_displayable_image(cv::Mat const& input_mat) {
+  if (input_mat.empty()) {
+    return {};
+  }
+  cv::Mat displayable;
+  input_mat.convertTo(displayable, CV_8U, vv::ImageConstants::kMaxPixelValue);
+  cv::cvtColor(displayable, displayable, cv::COLOR_GRAY2BGR);
+  return displayable;
+}
+}  // namespace
+
 namespace vv {
 namespace visualization {
 
 cv::Mat create_hog_images_row(vv::HOGResult const& hog_result) {
-  cv::Mat hog_magnitude_display;
-  cv::Mat hog_magnitude_filtered_display;
-
-  if (!hog_result.magnitude.empty()) {
-    hog_result.magnitude.convertTo(hog_magnitude_display, CV_8U,
-                                   vv::ImageConstants::kMaxPixelValue);
-    cv::cvtColor(hog_magnitude_display, hog_magnitude_display,
-                 cv::COLOR_GRAY2BGR);
-  }
-  if (!hog_result.magnitude_filtered.empty()) {
-    hog_result.magnitude_filtered.convertTo(hog_magnitude_filtered_display,
-                                            CV_8U,
-                                            vv::ImageConstants::kMaxPixelValue);
-    cv::cvtColor(hog_magnitude_filtered_display, hog_magnitude_filtered_display,
-                 cv::COLOR_GRAY2BGR);
-  }
+  cv::Mat hog_magnitude_display =
+      convert_to_displayable_image(hog_result.magnitude);
+  cv::Mat hog_magnitude_filtered_display =
+      convert_to_displayable_image(hog_result.magnitude_filtered);
 
   cv::Mat middle_row;
   if (!hog_magnitude_display.empty() &&
