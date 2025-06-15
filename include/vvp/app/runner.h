2a627b0b203c45cc240986f2d41445f7e4d39433
taehun-kmu <taehun@kookmin.ac.kr>
2025-06-15 20:03:31 +0900

    feat: Add Runner class header with PIMPL idiom

---
+++ include/vvp/app/runner.h
@@ -0,0 +1,27 @@
+#ifndef VVP_APP_RUNNER_H_
+#define VVP_APP_RUNNER_H_
+
+#include <memory>  // For std::unique_ptr
+
+namespace vv {
+
+class Runner {
+ public:
+  Runner(int argc, char* argv[]);
+  ~Runner();  // Required for pImpl with std::unique_ptr
+
+  Runner(Runner const&) = delete;
+  Runner& operator=(Runner const&) = delete;
+  Runner(Runner&&) noexcept = default;
+  Runner& operator=(Runner&&) noexcept = default;
+
+  auto Run() -> int;
+
+ private:
+  class Impl;  // Forward declaration for pImpl
+  std::unique_ptr<Impl> pimpl_;
+};
+
+}  // namespace vv
+
+#endif  // VVP_APP_RUNNER_H_
